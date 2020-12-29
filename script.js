fad2e91f4be3b3870baac13a91b7dfe3c43f79da
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2020-12-29 17:04:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const wrappers = document.querySelectorAll(".inputWrapper");
+
+for (let wrapper of wrappers) {
+  const formInput = wrapper.querySelector(".formInput");
+  formInput.addEventListener("invalid", function (e) {
+    e.preventDefault();
+  });
+}
+
+const submitButton = document.querySelector(`.claimButton`);
+submitButton.addEventListener("click", function () {
+  for (let wrapper of wrappers) {
+    const formInput = wrapper.querySelector(".formInput");
+    const errorImg = wrapper.querySelector(".errorImg");
+    const errorMessage = wrapper.querySelector(".errorMessage");
+
+    if (!formInput.value) {
+      errorImg.classList.remove("hidden");
+      errorMessage.classList.remove("hidden");
+      formInput.classList.add("errorState");
+    } else {
+      errorImg.classList.add("hidden");
+      errorMessage.classList.add("hidden");
+      formInput.classList.remove("errorState");
+    }
+  }
+});
+
